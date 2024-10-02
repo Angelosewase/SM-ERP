4794248104806fde7a450037c86aa0dafaa22263
Angelosewase <sewasejo8@gmail.com>
2024-10-02 16:23:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+    <div className="w-100vw h-100vh flex items-center justify-center">
+    <p>hello world</p>
+    </div>
+ 
+    </>
+  );
+}
+
+export default App;
